0a2fbfbf54ba61cbdec0ff90b0d679b4ff18d687
Anthony <anthonyfdez23@gmail.com>
2022-03-01 13:51:46 -0500

    started making a home page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SortedUp from "./screens/sortedUp";
 import SortedOldest from "./screens/oldest";
+import Home from "./screens/home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="" element={<Home />} />
         <Route path="sorted/up" element={<SortedUp />} />
         <Route path="sorted/oldest" element={<SortedOldest />} />
       </Routes>
