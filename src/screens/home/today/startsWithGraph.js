25b9ac4365615c33b1aab44488016edb5f6eaa39
Anthony <anthonyfdez23@gmail.com>
2022-03-03 09:50:20 -0500

    fixed ui

--- src/screens/home/today/startsWithGraph.js
+++ src/screens/home/today/startsWithGraph.js
@@ -10,9 +10,13 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
 const StartsWithGraph = ({ data }) => {
   const [today, setToday] = useState(null);
   const [tomorrow, setTomorrow] = useState(null);
+  const [isModalShown, setIsModalShown] = useState(false);
 
   useEffect(() => {
     var date = new Date();
@@ -59,16 +63,60 @@ const StartsWithGraph = ({ data }) => {
     }
   }, [data]);
 
+  const modalComponent = () => {
+    return (
+      <Modal show={isModalShown} onHide={() => setIsModalShown(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Como leer la grafica?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>X: Numero</p>
+          <p>
+            Y: Veces que ha salido un numero que empieza con correspondiente X
+          </p>
+          <br></br>
+          <h4>Explicacion en detalle</h4>
+          <p>
+            La grafica es para ayudar a ver los numeros que han salido que
+            empiezan con cada numero en el dia de hoy y manana
+          </p>
+          <br></br>
+          <p>
+            Por ejemplo ver que han salido numeros que empiezan con 4, 5 veces
+            los lunes, comparado con la grafica de abajo que muestra el martes
+          </p>
+          <br></br>
+          <p>
+            El eje de las Y, o lo alto que sube la grafica muestra la cantidad
+            de veces que ha salido un numero en las ultimas 56 semanas
+          </p>
+          <br></br>
+          <p>
+            Cada numero mostrado en el eje de las X, o horizontal en la grafica
+            (va de 0 a 9), representa el primer numero de cada numero de 0 a 99
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => setIsModalShown(false)}>
+            Cerrar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
   if (today && tomorrow) {
     return (
       <div style={{ width: "100%" }}>
-        <p>
-          Las graficas muestran en el eje de las Y la cantidad de veces que has
-          salido un numero que empieza con el numero que le corresponde en el
-          eje de las X
+        {modalComponent()}
+        <p>Grafica muestra 56 semanas de historial de numeros</p>
+        <br></br>
+        <p onClick={() => setIsModalShown(true)} className="link">
+          Como entender la grafica?
         </p>
         <br></br>
-        <p>Hoy</p>
+        <h3>Hoy</h3>
+
         <ResponsiveContainer width="100%" height={200}>
           <AreaChart
             width={500}
@@ -94,7 +142,7 @@ const StartsWithGraph = ({ data }) => {
             />
           </AreaChart>
         </ResponsiveContainer>
-        <p>Manana</p>
+        <h3>Manana</h3>
         <ResponsiveContainer width="100%" height={200}>
           <AreaChart
             width={500}
